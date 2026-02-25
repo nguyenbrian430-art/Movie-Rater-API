c1299b057a6776e81483276b108862c82798187f
nguyenbrian430-art <nguyenbrian430@gmail.com>
2026-02-25 15:43:25 -0500

    put in new local port

--- movierater/settings.py
+++ movierater/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&=^6&ln!t_i8#vx6)v#srni)6t=qhe_j6m+*zk^-c-!j7jw(hl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.1.165"]
 
 
 # Application definition
